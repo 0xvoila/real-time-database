1a4adaba682e502510792553372eb9b6a800d191
Amit Aggarwal <amit.a@shawacademy.com>
2017-07-23 21:28:09 +0530

    refactoring code with async

--- src/database.js
+++ src/database.js
@@ -1,97 +1,89 @@
 // This module contains database functions
 var helper = require('./helper.js');
 var request = require('request');
+var async = require('async');
 
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://root:2June1989!@voila-cluster-shard-00-00-45vfv.mongodb.net:27017,voila-cluster-shard-00-01-45vfv.mongodb.net:27017,voila-cluster-shard-00-02-45vfv.mongodb.net:27017/test?ssl=true&replicaSet=voila-cluster-shard-0&authSource=admin'
 
 exports.insertLeaf = function(path,element,value){
-  MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-    var myobj = { abs_path: path, element: element , value: value };
-    db.collection("test").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    db.close();
-  });
+  async.waterfall([
+    function(callback){
+      MongoClient.connect(url,callback);
+    },
+    function(db,callback){
+      var firebaseRecord = { abs_path: path, element: element , value: value };
+      db.collection("test").insertOne(firebaseRecord, callback);
+    }
+  ],
+    function(error,result){
+      if (error) throw error;
+    }
+  )
+};
 
-  });
-}
 
 exports.deleteSubTree = function(reference){
-  console.log("delteing sub tree");
-  MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-    var myobj = { abs_path: new RegExp( '^' + reference)};
-    db.collection("test").remove(myobj, function(err, res) {
-      if (err) throw err;
-      db.close();
-    });
-  });
+  async.waterfall([
+    function(callback){
+      MongoClient.connect(url,callback);
+    },
+    function(db,callback){
+      var firebaseRecord = { abs_path: new RegExp( '^' + reference)};
+      db.collection("test").remove(firebaseRecord, callback);
+    }
+  ],
+    function(error,result){
+      if (error) throw error;
+    }
+  )
 }
 
 exports.updateLeaf = function(path,element,value){
-  MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-    var queryObj = {abs_path: path, element:element}
-    var myobj = { abs_path:path, element:element,value: value };
-    console.log(queryObj);
-    db.collection("test").updateOne(queryObj,myobj, {upsert:true} ,function(err, res) {
-      if (err) throw err;
-      db.close();
-    });
-  });
-}
-
-exports.deleteData = function(path,value){
-  MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-    var myobj = { path: path, value: value };
-    db.collection("test").updateOne(myobj, function(err, res) {
-      if (err) throw err;
-
-      // now create the snapshot of the data
-      createSnapshot(res.insertedId,path)
-      db.close();
-    });
-  });
+  async.waterfall([
+    function(callback){
+      MongoClient.connect(url,callback);
+    },
+    function(db,callback){
+      var queryObj = {abs_path: path, element:element}
+      var firebaseRecord = { abs_path: path, element: element , value: value };
+      db.collection("test").updateOne(queryObj,firebaseRecord, {upsert:true} ,callback);
+    }
+  ],
+    function(error,result){
+      if (error) throw error;
+    }
+  )
 }
 
 exports.createSnapshot = function(path){
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var findObj = { abs_path: new RegExp("^" + path)};
-    console.log(findObj);
-    db.collection("test").find(findObj).snapshot().toArray(function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      helper.postUpdates(path,{"body":res});
-      db.close();
-    });
-  });
+
+  async.waterfall([
+    function(callback){
+      MongoClient.connect(url,callback);
+    },
+
+    function(db,callback){
+      var firebaseRecord = { abs_path: new RegExp("^" + path)};
+      db.collection("test").find(firebaseRecord).snapshot().toArray(callback);
+    }
+  ],
+    function(error,result){
+      if (error) throw error;
+      return result;
+    }
+  )
+
+  // MongoClient.connect(url, function(err, db) {
+  //   if (err) throw err;
+  //   var findObj = { abs_path: new RegExp("^" + path)};
+  //   console.log(findObj);
+  //   db.collection("test").find(findObj).snapshot().toArray(function(err, res) {
+  //     if (err) throw err;
+  //     console.log(res);
+  //     helper.postUpdates(path,{"body":res});
+  //     db.close();
+  //   });
+  // });
 }
 
-var isLocationAnArray = function(path){
-  var arr = [];
-  getChildrenAtLocation (path, function(result){
-    for( child in result){
-      arr.push(child);
-    }
-    if(isFirebaseArray(arr)){
-      return true;
-    }
-    else {
-      return false;
-    }
-  })
-}
-
-
-var isFirebaseArray = function(arr){
-
-  for(var i=1; i<arr.length;i++)
-    {
-        if(arr[i-1]>arr[i])
-            return false;
-    }
-    return true;
-}
