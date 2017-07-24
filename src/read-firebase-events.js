8a425ca1b17971d4f82d58ded8e5f3d3440d3a55
Amit Aggarwal <amit.a@shawacademy.com>
2017-07-25 01:55:13 +0530

    working on post updates

--- src/read-firebase-events.js
+++ src/read-firebase-events.js
@@ -5,6 +5,45 @@ var database = require('./database.js');
 var async = require('async');
 var request = require('request');
 
+var postUpdates = function(databasesnapshot, _callback){
+  console.log("posting updates");
+   var url = 'http://13.126.96.13/updates/'
+            var options = {
+              body: databasesnapshot,
+              json: true,
+              url: url
+            }
+            request.post(options,function(error,response,body){
+              _callback(error,response,body);
+            });
+}
+
+var splitPathListFunc = function(eventArray, _callback){
+  console.log("splitting paths");
+  var splitPathList = [];
+  async.each(eventArray, function(eve, eventCallback){
+     var absPathArray = eve.abs_path.split(",");
+      var eventType = eve.event_type;
+
+      var absPathArrayLen = absPathArray.length;
+      for(var i=0;i<absPathArrayLen;i++){
+        splitPathList.push(absPathArray);
+        absPathArray.pop();
+      }
+      eventCallback();
+
+  }, function(error,result){
+     _callback(null,splitPathList)
+  })
+}
+
+var createDatabaseSnapShot = function(absPath, _callback){
+  console.log("creating snapshot");
+  database.createSnapshot(absPath,null,null, function(error, result){
+    _callback(null,result);
+  });
+}
+
 exports.handler = (event, context, globalCallback) => {
 
     var eventArray = [];
@@ -14,50 +53,26 @@ exports.handler = (event, context, globalCallback) => {
         var abs_path = jsonPayload.abs_path;
         var event_type = jsonPayload.event_type;
         eventArray.push({abs_path:abs_path, event_type:event_type})
+        console.log(eventArray);
         callback(null);
 
     }, function(error, result){
       if(error) throw error;
     })
 
-    async.each(eventArray, function(eve, eventCallback){
+    splitPathListFunc(eventArray, function(error, splitPathList){
 
-      var absPathArray = eve.abs_path.split(",");
-      var eventType = eve.event_type;
+       async.each(splitPathList, function(absPath, forEachCallback){
 
-      var absPathArrayLen = absPathArray.length;
-      var splitPathList = [];
-      for(var i=0;i<absPathArrayLen;i++){
-        splitPathList.push(absPathArray);
-        absPathArray.pop();
-      }
-
-      async.each(splitPathList, function(absPath, forEachCallback){
-
-          async.waterfall([function(callback){
-            database.createSnapshot(absPath,null,null, callback);
-          }, function(datasnapshot, callback){
-            //console.log(datasnapshot);
-            var url = 'http://13.126.96.13/updates/'
-            var options = {
-              body: datasnapshot,
-              json: true,
-              url: url
-            }
-            request.post(options,function(){
-              callback(null);
-            });
-          }], function(error, result){
-            if(error) throw error;
-            forEachCallback(null);
+          createDatabaseSnapShot(absPath, function(error, snapshot){
+            postUpdates(snapshot, function(error,response, body){
+              globalCallback(null)
+            })
           })
-      }, function(error, result){
-        if(error) throw error;
-        eventCallback(null);
-      }) // async.each close
-    }, function(error, result){
-      if(error) throw error;
-      globalCallback(null);
+       });
     })
 };
+
+
+
 
