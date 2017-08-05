85fb24d9141a4013df6f28fc9ba43e1e2116cb8c
Amit Aggarwal <amit.a@shawacademy.com>
2017-08-05 16:20:47 +0530

    fetching data on frontend

--- src/web-server/MyController.js
+++ src/web-server/MyController.js
@@ -2,31 +2,25 @@ var myApp = angular.module('myApp',[]);
 
 var myController = myApp.controller('myController',function(firebaseService){
 
+  $scope.messages = null;
+
   firebaseService.ref("/messages", 'child_added', function(error, data){
     if(error){
       throw error;
       return
     }
     else{
-      console.log(data);
+      $scope.messages.push(data)
     }
 
   })
 
 
-  for( var i=0; i<100;i++){
+  for( var i=0; i<10;i++){
 
      json = {
-          "users": {
-            "amit": {
-              "my_date_of_birth": "June 23, 1912",
-              "full_name": "Alan Turing"
-            },
-            "gracehop": {
-              "date_of_birth": "December 9, 1906",
-              "full_name": "Grace Hopper",
-            }
-          }
+          "chat_room_id" : i,
+          "body" : i
         }
 
      firebaseService.push("/messages",json);
