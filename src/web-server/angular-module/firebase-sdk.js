0c79f79c4b5f470fc5b61b5eb20767c31f083082
Amit Aggarwal <amit.a@shawacademy.com>
2017-08-09 20:42:23 +0530

    working on firebase sdk

--- src/web-server/angular-module/firebase-sdk.js
+++ src/web-server/angular-module/firebase-sdk.js
@@ -2,17 +2,33 @@ var myApp = angular.module('myApp');
 
 myApp.service("firebaseService", function($http){
 
-  this.ref = function(absolutePath,event_type,callback){
-    var socket = io('http://54.83.184.40');
-    socket.emit('join_room', { abs_path: absolutePath, event_type :event_type});
-    socket.on("new_data", function(data){
-      $http.post('http://54.83.184.40/get',  data).then(function(data){
-        callback(null,data.data);
-      }, function(error){
-        callback(error)
-      });
-    })
+  this.socket = null;
+
+   this.ref = function(absolutePath,event_type,callback){
+
+    if(!this.socket){
+      console.log("connection already exists")
+      this.database()
+    }
+
+    this.socket.emit('join_room', { abs_path: absolutePath, event_type :event_type});
+    this.socket.on("new_data", function(data){
+    $http.post('http://54.83.184.40/get',  data).then(function(data){
+      callback(null,data.data);
+    }, function(error){
+      callback(error)
+    });
+  })
     return this
+  }
+
+  this.database = function(){
+
+     if(!this.socket){
+      this.socket = io('http://54.83.184.40');
+     }
+
+     return this.ref
   }
 
   this.set = function(absolutePath,data,callback){
