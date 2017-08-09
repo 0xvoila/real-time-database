f3f225044145d3f2294348a177083cff73f31427
Amit Aggarwal <amit.a@shawacademy.com>
2017-08-10 00:45:15 +0530

    changing to firebase.shawacademy.com

--- src/web-server/angular-module/firebase-sdk.js
+++ src/web-server/angular-module/firebase-sdk.js
@@ -7,7 +7,7 @@ myApp.service("firebaseService", function($http){
 
      var _this = this
      if(!this.socket){
-      _this.socket = io('http://54.83.184.40');
+      _this.socket = io('http://firebase.shawacademy.com');
      }
      return new function(){
         this.ref = function(reference){
@@ -17,7 +17,7 @@ myApp.service("firebaseService", function($http){
             this.on = function(event,callback){
               _this.socket.emit('join_room', { abs_path: _this.reference, event_type :event});
                 _this.socket.on("new_data", function(data){
-                    $http.post('http://54.83.184.40/get',  data).then(function(data){
+                    $http.post('http://firebase.shawacademy.com/get',  data).then(function(data){
                       callback(null,data.data);
                     }, function(error){
                       callback(error)
@@ -32,17 +32,17 @@ myApp.service("firebaseService", function($http){
 
   this.set = function(absolutePath,data,callback){
     var transportObj = {reference:absolutePath, body:data};
-    $http.post('http://54.83.184.40/set',  transportObj);
+    $http.post('http://firebase.shawacademy.com/set',  transportObj);
   }
 
   this.update = function(absolutePath, data, callback){
     var transportObj = {reference:absolutePath, body:data};
-    $http.post('http://54.83.184.40/update',  transportObj);
+    $http.post('http://firebase.shawacademy.com/update',  transportObj);
   }
 
   this.push = function(absolutePath, data, callback){
     var transportObj = {reference:absolutePath, body:data};
-    $http.post('http://54.83.184.40/push',  transportObj);
+    $http.post('http://firebase.shawacademy.com/push',  transportObj);
   }
 
 })
