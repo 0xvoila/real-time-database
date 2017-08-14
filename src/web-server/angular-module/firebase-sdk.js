d681dcb75d5a5a458e6927cc837d6ec32656fe2d
Amit Aggarwal <amit.a@shawacademy.com>
2017-08-14 09:43:36 +0530

    working on once

--- src/web-server/angular-module/firebase-sdk.js
+++ src/web-server/angular-module/firebase-sdk.js
@@ -21,9 +21,8 @@ myApp.service("firebaseService", function($http){
 
           return new function(){
             this.on = function(event,callback){
-              _this.onNSP.on("connection", function(){
-                   _this.onNSP.emit('on', {absolute_path: _this.reference, event_type :event});
-                   _this.onNSP.on("onData", function(data){
+              _this.onNSP.emit('on', {absolute_path: _this.reference, event_type :event});
+                _this.onNSP.on("onData", function(data){
                     $http.post('http://firebase.shawacademy.com/get',  data).then(function(data){
                       //_this.onNSP.emit("off",{absolute_path:_this.reference, event_type:event})
                       callback(null,data.data);
@@ -31,12 +30,9 @@ myApp.service("firebaseService", function($http){
                         callback(error)
                     });
                   })
-              }) // on connection only
-
             },
             this.once = function(event,callback){
-              _this.onceNSP.on("connection", function(){
-                _this.onceNSP.emit('once', {absolute_path: _this.reference, event_type :event});
+              _this.onceNSP.emit('once', {absolute_path: _this.reference, event_type :event});
                 _this.onceNSP.on("onceData", function(data){
                     $http.post('http://firebase.shawacademy.com/get',  data).then(function(data){
                       _this.onceNSP.emit("off",{absolute_path:_this.reference, event_type:event})
@@ -45,7 +41,6 @@ myApp.service("firebaseService", function($http){
                       callback(error)
                     });
                 })
-              }) // on connection only
             }
           }
         }
