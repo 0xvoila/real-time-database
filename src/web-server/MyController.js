f98cb1258f78be6334fcfd7b5a9590507c06aced
Amit Aggarwal <amit.a@shawacademy.com>
2017-08-15 16:45:26 +0530

    working on some chat feature

--- src/web-server/MyController.js
+++ src/web-server/MyController.js
@@ -3,73 +3,9 @@ var myApp = angular.module('myApp',[]);
 var myController = myApp.controller('myController',function($scope,firebaseService){
 
   $scope.messages = [];
-  $scope.mymessages = [];
-  $scope.myData = null
+  $scope.thingToSay = ""
 
-  function gup(name) {
-      name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-      var regexS = "[\\?&]"+name+"=([^&#]*)";
-      var regex = new RegExp( regexS );
-      var results = regex.exec( window.location.href );
-      if( results == null )
-          return null;
-      else
-          return results[1];
-  }
-
-  if(gup("type") == "get" || gup("type") == "all"){
-
-    // firebaseService.database().ref("/messages").once('child_added', function(error, data){
-    //   if(error){
-    //     throw error;
-    //     return
-    //   }
-    //   else{
-    //     $scope.myData = data["/"]["messages"]
-    //   }
-
-    // });
-
-    // var ref = firebaseService.database().ref("/messages")
-    // ref.on('value', function(error, data){
-    //   if(error){
-    //     throw error;
-    //     return
-    //   }
-    //   else{
-    //     $scope.messages.push(data)
-    //   }
-
-    // });
-
-    //ref.off()
-
-    var ref = firebaseService.database().ref("/messages")
-    ref.on('child_added', function(error, data){
-      console.log("caling callback again")
-      if(error){
-        throw error;
-        return
-      }
-      else{
-        $scope.mymessages.push(data)
-      }
-
-    });
-
-  }
-
-  if(gup("type") == "push" || gup("type") == "all"){
-    var i=0;
-    setInterval(function(){
-       json = {
-            "chat_room_id" : "goog",
-            "body" : "amit",
-            "counter": i
-          }
-
-       firebaseService.push("/messages",json);
-       i++;
-     },10000);
+  $scope.submitChat = function(){
+    console.log("chat is " + thingToSay)
   }
 });
