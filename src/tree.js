03dc8f387fd79cd11d9f1fdd0543ca43715b0073
Amit Aggarwal <amit.a@shawacademy.com>
2017-08-19 07:44:13 +0530

    working on fixing events issues

--- src/tree.js
+++ src/tree.js
@@ -173,14 +173,14 @@ var Tree = function(){
     }
 
     if(eventJson.grandParents && parentNode && parentNode.parent){
-      var grandParent = parentNode.parent
-      while(grandParent != null){
-        for(var i=0;i<eventJson.grandParents.length;i++){
-          var event = eventJson.grandParent[i]
-          var eventHash = md5(grandParent.data.key + event)
-          grandParent.events[eventHash] = {data_url:grandParent.data.key,event:event}
+      var grandParentNode = parentNode.parent
+      while(grandParentNode != null){
+        for(var i=0;i<eventJson.grandParentNodes.length;i++){
+          var event = eventJson.grandParentNode[i]
+          var eventHash = md5(grandParentNode.data.key + event)
+          grandParentNode.events[eventHash] = {data_url:grandParentNode.data.key,event:event}
         }
-        grandParent = grandParent.parent;
+        grandParentNode = grandParentNode.parent;
       }
     }
 
